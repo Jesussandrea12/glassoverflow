9069cda0eeb3fe48a5a9b1e829a214958300e25b
jesussandrea <jesussandrea12@gmail.com>
2017-12-19 09:52:04 -0400

    [Todo chevere, falta agregar mongodb]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'app';
+
+  constructor(private authService: AuthService) {}
+
+  fullName() {
+    return this.authService.currentUser.fullName();
+  }
+
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,84 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { MomentModule } from 'angular2-moment';
 import { AppComponent } from './app.component';
+
+// Material Components
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
-import { MomentModule } from 'angular2-moment';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Global services
+import { AuthService } from './auth/auth.service';
+
+// Routing
+import { Routing } from './app.routing';
 
 // Material Angular
-import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCheckboxModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+
+// Screens
+import { SigninScreenComponent } from './auth/signin.screen';
+import { SignupScreenComponent } from './auth/signup.screen';
+import { QuestionScreenComponent } from './question/question.screen';
+
 // Componentes
-import { QuestionDetailComponent } from './question-detail/question-detail.component';
-import { AnswerFormComponent } from './answer-form/answer-form.component';
-import { SigninComponent } from './auth/signin.component';
+import { QuestionListComponent } from './question/question-list.component';
+import { QuestionDetailComponent } from './question/question-detail.component';
+import { QuestionFormComponent } from './question/question-form.component';
+import { AnswerFormComponent } from './answer/answer-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionDetailComponent,
     AnswerFormComponent,
-    SigninComponent
+    SigninScreenComponent,
+    SignupScreenComponent,
+    QuestionListComponent,
+    QuestionScreenComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MomentModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Routing,
+    HttpModule,
+    // Material Angular
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
     MatCheckboxModule,
     MatCardModule,
     MatInputModule,
-    MomentModule,
-    FormsModule,
-    ReactiveFormsModule
+    MatListModule,
+    MatGridListModule,
+    MatRadioModule,
+    MatButtonToggleModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
